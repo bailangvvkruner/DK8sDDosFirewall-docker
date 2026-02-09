ac865dc57c90b0caa627f13a6ddc77483d4fdce9
bailangvvkg <bailangvvking@outlook.com>
2026-02-09 17:38:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,41 @@
 #!/bin/sh
-set -e
+set -eux
 
-# 检查 HTTP_PORT 是否设置，否则默认为 80
-if [ -z "$HTTP_PORT" ]; then
-  echo "--> HTTP_PORT not set, defaulting to 80"
-  TARGET_PORT=80
-else
-  echo "--> HTTP_PORT is set to: $HTTP_PORT"
-  TARGET_PORT=$HTTP_PORT
-fi
+openssl req \
+  -x509 \
+  -new \
+  -nodes \
+  -newkey ec:<(openssl ecparam -name prime256v1) \
+  -keyout default.key \
+  -out default.crt \
+  -sha256 \
+  -days 3650 \
+  -subj "/CN=_" \
+  -addext "subjectAltName = DNS:_"
 
-# 创建一个临时文件
-CONF_TMP=$(mktemp)
+# # 检查 HTTP_PORT 是否设置，否则默认为 80
+# if [ -z "$HTTP_PORT" ]; then
+#   echo "--> HTTP_PORT not set, defaulting to 80"
+#   TARGET_PORT=80
+# else
+#   echo "--> HTTP_PORT is set to: $HTTP_PORT"
+#   TARGET_PORT=$HTTP_PORT
+# fi
 
-# 使用 awk 替换监听端口。这比 sed 更健壮。
-awk -v port="$TARGET_PORT" '{gsub(/listen\s+[0-9]+;/, "listen " port ";")}1' /etc/nginx/nginx.conf > "$CONF_TMP"
+# # 创建一个临时文件
+# CONF_TMP=$(mktemp)
 
-# 将修改后的内容写回到配置文件，然后删除临时文件
-cat "$CONF_TMP" > /etc/nginx/nginx.conf && rm "$CONF_TMP"
+# # 使用 awk 替换监听端口。这比 sed 更健壮。
+# awk -v port="$TARGET_PORT" '{gsub(/listen\s+[0-9]+;/, "listen " port ";")}1' /etc/nginx/nginx.conf > "$CONF_TMP"
 
-# echo "--> Nginx configuration updated to listen on port $TARGET_PORT. New configuration:"
-# echo "--------------------"
-# cat /etc/nginx/nginx.conf
-# echo "--------------------"
+# # 将修改后的内容写回到配置文件，然后删除临时文件
+# cat "$CONF_TMP" > /etc/nginx/nginx.conf && rm "$CONF_TMP"
 
-echo "--> Starting Nginx..."
-# 执行传递给脚本的命令 (例如, nginx -g 'daemon off;')
-exec "$@"
+# # echo "--> Nginx configuration updated to listen on port $TARGET_PORT. New configuration:"
+# # echo "--------------------"
+# # cat /etc/nginx/nginx.conf
+# # echo "--------------------"
+
+# echo "--> Starting Nginx..."
+# # 执行传递给脚本的命令 (例如, nginx -g 'daemon off;')
+# exec "$@"
